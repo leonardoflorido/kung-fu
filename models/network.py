02e6177b8a886f739c4075bfaef3d4dccc54d179
Leonardo Flórido <leonardoflorido@proton.me>
2024-09-02 16:54:13 +0100

    Update the network.

--- models/network.py
+++ models/network.py
@@ -16,4 +16,10 @@ from gymnasium.spaces import Box
 class Network(nn.Module):
     def __init__(self, action_size):
         super(Network, self).__init__()
-        self.conv1 = torch.nn.Conv2d(in_channels = 4, out_channels = 32)
+        self.conv1 = torch.nn.Conv2d(in_channels = 4, out_channels = 32, kernel_size = (3,3), stride = 2)
+        self.conv2 = torch.nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = (3,3), stride = 2)
+        self.conv3 = torch.nn.Conv2d(in_channels = 32, out_channels = 32, kernel_size = (3,3), stride = 2)
+        self.flatten = torch.nn.Flatten()
+        self.fc1 = torch.nn.Linear(512, 128)
+        self.fc2a = torch.nn.Linear(128, action_size)
+        self.fc2s = torch.nn.Linear(128, 1)
